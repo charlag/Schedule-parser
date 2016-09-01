4fda0a8b1561dede0580f0ce8d199aac11acb8cd
Ivan Kupalov <ivan.kupalov@icloud.com>
2016-09-01 16:35:56 +0300

    Add telegram bot

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'spreadsheet'
 gem 'icalendar'
+gem 'telegram-bot-ruby'
---
+++ bot.rb
@@ -0,0 +1,15 @@
+require 'telegram/bot'
+
+token = '254014979:AAGBL-jGLAhatmNiStcY5zD6jcgiPFbd65g'
+
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
